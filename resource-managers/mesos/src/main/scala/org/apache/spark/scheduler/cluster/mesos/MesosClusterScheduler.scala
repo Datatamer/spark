ae9b9eb374968cec19684735207a77331db1089f
Timothy Danford <tdanford@gmail.com>
2017-12-27 14:08:17 -0500

    Added entrypoint to mesos cluster scheduler docker command.

--- resource-managers/mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterScheduler.scala
+++ resource-managers/mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterScheduler.scala
@@ -409,7 +409,7 @@ private[spark] class MesosClusterScheduler(
     val (executable, sandboxPath) = if (dockerDefined) {
       // Application jar is automatically downloaded in the mounted sandbox by Mesos,
       // and the path to the mounted volume is stored in $MESOS_SANDBOX env variable.
-      ("./bin/spark-submit", "$MESOS_SANDBOX")
+      ("./entrypoint.sh && ./bin/spark-submit", "$MESOS_SANDBOX")
     } else if (executorUri.isDefined) {
       val folderBasename = executorUri.get.split('/').last.split('.').head
 
